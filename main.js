d15e941ebd08550e71383f9b9e152989ed483dad
Shehabmohamed86 <shehabmohamed8688@gmail.com>
2022-08-18 16:12:12 -0400

    adviceproject

--- main.js
+++ main.js
@@ -5,30 +5,28 @@
   const dots = document.querySelector('.img-2');
   const num =  document.querySelector('.num');
   const api = 'https://api.adviceslip.com/advice';
-  fetch(api)
-  .then(response=>{
-    return response.json();
-  })
-  .then(data=>{
-     console.log(data)
-     const {id,advice} =  data.slip;
-     dots.addEventListener('click',()=>{
+  
+    
+ function getAdvice(){
+    fetch(api)
+    .then(response=>{
+      return response.json();
+    })
+    .then(data=>{
+      const  {id,advice} =  data.slip;
       dots.classList.toggle('animation')
       adviceText.textContent = advice
-      reload()
-
-     })
-     dots.addEventListener('click',()=>{
       num.textContent = id
-     })
-  
-  })
-
-  function reload(){
-    dots.addEventListener('click',()=>{
-      window.location.reload()
-    })
+   })
   }
+  
+  
+  
+  
+  dots.addEventListener('click',getAdvice)
+
+  
+   
  
    
 
